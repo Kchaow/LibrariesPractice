37642fc0db99dbfadc5b6f0c9c6affeddc072bbe
Alexey Letunov <xnihonsamuraix@gmail.com>
2024-06-12 12:44:40 +0300

    matplotlib practice

--- matplotlibPractice.py
+++ matplotlibPractice.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.gridspec import GridSpec
+from matplotlib.ticker import (NullLocator, LinearLocator, MultipleLocator, IndexLocator,
+                               FixedLocator, LogLocator, MaxNLocator)
 
 #Использование plot
 plt.plot([1, 2, -6, 0, 4]) #y - указанные значения, x - индексы элементов
@@ -113,5 +115,91 @@ ax2 = fig.add_subplot(gs[1, 0:2])
 ax2.plot(np.random.random(10))
 ax3 = fig.add_subplot(gs[:,2])
 ax3.plot(np.random.random(10))
+
+plt.show()
+
+#Граничные значения
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+ax.set(xlim=(-5, 30), ylim=(-1, 6))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+ax.set_xlim(xmin=-5, xmax=10)
+ax.set_ylim(-1, 6)
+
+plt.show()
+
+#Управление метками
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+lc = NullLocator()
+
+ax.grid()
+ax.yaxis.set_major_locator(lc)
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+ax.grid()
+ax.yaxis.set_major_locator(LinearLocator(5))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+ax.grid()
+ax.yaxis.set_major_locator(MultipleLocator(2))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+x = np.arange(-np.pi/2, np.pi, 0.1)
+ax.plot(x, np.sin(x))
+
+ax.grid()
+ax.yaxis.set_major_locator(IndexLocator(base=0.5, offset=0.57))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+ax.grid()
+ax.yaxis.set_major_locator(FixedLocator([1, 4, 9, 12]))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+ax.grid()
+ax.yaxis.set_major_locator(LogLocator(base=2))
+
+plt.show()
+
+fig = plt.figure(figsize=(7, 4))
+ax = fig.add_subplot()
+ax.plot(np.arange(1, 5, 0.25))
+
+ax.minorticks_on()
+ax.grid(which='major', lw=2)
+ax.grid(which='minor')
+
+ax.yaxis.set_major_locator(MaxNLocator(3))
 
 plt.show()
